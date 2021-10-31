837864a4ef51de9bc6a9f40a82b0d5b8fa08188f
Weronika Piotrowska <weronika.piotrowska.pl@gmail.com>
2021-10-31 11:41:29 +0100

    Added receiver

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Application.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Receiver _receiver;
+
+        public HomeController()
+        {
+            _receiver = new Receiver("localhost", "temperature");
+        }
+
+        public IActionResult Index()
+        {
+            List<String> messages = _receiver.GetMessages();
+            return View("Messages", messages);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+
+}
---
+++ Receiver.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Application
+{
+    public class Receiver
+    {
+        private string _hostName;
+        private string _queueName;
+        
+        public Receiver(string hostName, string queueName)
+        {
+            _hostName = hostName;
+            _queueName = queueName;
+        }
+        
+        public List<String> GetMessages()
+        {
+            var messages = new List<string>();
+
+            var factory = new ConnectionFactory() { HostName = _hostName };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: _queueName,
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body.ToArray());
+                        var log = JsonConvert.DeserializeObject<string>(message);
+                        messages.Add(log);
+
+                        Console.WriteLine(message);
+                    };
+
+                    channel.BasicConsume(queue: _queueName,
+                        consumer: consumer);
+                }
+            }
+            return messages;
+        }
+    }
+}
